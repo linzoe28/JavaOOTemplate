7f19b1ff739c095f34690afade663ac64155e9b0
linzoe <s1074802@g.yzu.edu.tw>
2020-12-30 05:59:22 +0000

    commit

--- src/main/java/FundamentalJavaTemplate/Main.java
+++ src/main/java/FundamentalJavaTemplate/Main.java
@@ -10,12 +10,12 @@ package FundamentalJavaTemplate;
  * @author lendle
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         System.out.println("Hello!");
     }
     
